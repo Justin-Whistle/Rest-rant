cb622ff8000431ec7309225f97fe7b3629c0bdc7
Justin-Whistle <92463485+Justin-Whistle@users.noreply.github.com>
2022-02-26 10:17:17 -0600

    finished and working rest-rant3

--- views/places/index.jsx
+++ views/places/index.jsx
@@ -4,9 +4,9 @@ const Def = require('./default')
 function index (data) {
     let placesFormatted = data.places.map((place) => {
       return (
-        <div>
+        <div> key={place.name}
           <h2>{place.name}</h2>
-          <img src={place.pic} alt={place.name}>
+          <img src={place.pic} alt={place.name} />
         </div>
       )
     })
