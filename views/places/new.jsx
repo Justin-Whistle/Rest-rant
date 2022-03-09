fa6b1cabd824243c283436bbd7ebdf3321fe0478
Justin-Whistle <92463485+Justin-Whistle@users.noreply.github.com>
2022-03-08 18:44:49 -0600

    finished part 9 not working

--- views/places/new.jsx
+++ views/places/new.jsx
@@ -29,7 +29,7 @@ function new_form () {
                     </div>
                     <div className="form-group mx-auto col-sm-6 col-md-4 col-lg-3">
                         <label for="founded">Founded Year</label>
-                        <input className="form-control" id="founded" name="founded" />
+                        <input type="number" className="form-control" id="founded" name="founded" value={new Date().getFullYear()} />
                     </div>
                     <input className="btn btn-primary" type="submit" value="Add Place" />
                 </form>
